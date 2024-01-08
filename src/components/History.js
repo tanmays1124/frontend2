8f8bfc4858c996f599c92734593de8fdbd51d667
Tanmay <tanmays1124@gmail.com>
2024-01-09 00:49:26 +0530

    making history page dynamic

--- src/components/History.js
+++ src/components/History.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import bg1 from '../images/history.jpg';
 import Navbar from './Navbar';
 
-const History = () => {
+const History = ({userId, setUserId}) => {
     const styles = {
         background: {
             backgroundImage: `url(${bg1})`,
@@ -34,15 +36,55 @@ const History = () => {
         }
     };
 
+    const [questionHistory, setQuestionHistory] = useState({});
+    const [domain, setDomain] = useState([]);
+    const [difficultyLevel, setDifficultyLevel] = useState([]);
+    const [score, setScore] = useState();
+    const [attemptedQuestion, setAttemptedQuestion] = useState([]);
+    const [submissionTime,setSubmissionTime] = useState([]);
+
+
+    const fetchUserQuestionHistory = async () => {
+        try {
+          const response = await fetch(`http://127.0.0.1:8000/api/questionhistoryget/?user_id=${userId}`);
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const data = await response.json();
+
+
+          setQuestionHistory(data);
+          setDomain(data.map(item => item.domain));
+          setDifficultyLevel(data.map(item => item.difficulty_level));
+          setScore(data.map(item => item.score));
+          setAttemptedQuestion(data.map(item => item.attempted_questions));
+          setSubmissionTime(data.map(item => item.submission_time));
+
+
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUserQuestionHistory();
+  }, []);
+
+
+    //   const data = JSON.stringify(questionHistory, null)
     return (
         <>
         <Navbar page={'History'}/>
-        <div className="contained" style={styles.contained}>
-                    <div className='text' style={styles.text}>No history</div>
+         
+        {questionHistory.length==0 ?
 
+        (<div className="contained" style={styles.contained}>
+                    <div className='text' style={styles.text}>No history</div>
+                    
         <div className='background' style={styles.background}>
         </div>
-        </div>
+        </div>): (<div style={{marginTop:'100px'}}>{domain}{score}{difficultyLevel}{submissionTime}</div>)}
+        
         </>
     );
 }
